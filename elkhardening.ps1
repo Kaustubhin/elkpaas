217d9587ba3630d27a1920bc5f7ed6945ce8cc8e
Kaustubhin <kaustubhin@gmail.com>
2018-11-04 17:02:32 +0530

    Update elkhardening.ps1

--- elkhardening.ps1
+++ elkhardening.ps1
@@ -5,8 +5,7 @@ param
     [string[]]$NodeName = 'localhost'
     )
 # Import the module that defines custom resources
-Import-DscResource -ModuleName xPSDesiredStateConfiguration
-Import-DscResource -ModuleName xRemoteFile
+Import-DscResource -ModuleName PSDesiredStateConfiguration
 
 Node $NodeName
 {
@@ -29,19 +28,5 @@ Node $NodeName
         ValueData = '1'
     }
 
-    xRemoteFile javaInstaller {
-        DestinationPath = (Join-Path $localDscFileDir "Java\jreInstaller.exe")
-        Uri = "http://javadl.oracle.com/webapps/download/AutoDL?BundleId=211999"
-    }
-        Package java8
-    {
-        Ensure = 'Present'
-        Name = 'Java 8'
-        Path = (Join-Path $localDscFileDir "Java\jreInstaller.exe")
-        Arguments = '/s REBOOT=0 SPONSORS=0 REMOVEOUTOFDATEJRES=1 INSTALL_SILENT=1 AUTO_UPDATE=0 EULA=0 /l*v "C:\DscFiles\Java\jreInstaller.exe.log"' #From https://powershell.org/forums/topic/issue-installing-java-32-bit-using-package-resource/#post-39206
-        ProductId = '26A24AE4-039D-4CA4-87B4-2F64180101F0'
-        DependsOn = @("[xRemoteFile]javaInstaller")         
-    }
-
-}
+  }
 }
