622acd55f19ec7eec99bad3a7474cdbaf13f93d7
Kaustubh Oak <kaustubh.oak@globant.com>
2018-11-07 02:14:17 +0530

    Main pwsh

---
+++ azuredeploy.ps1
@@ -0,0 +1,29 @@
+$rg="KO002"
+
+New-AzureRmResourceGroup -Name $rg -Location "Central US"
+
+Write-Host "Enter the Username and Password for VM"
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rg `
+-TemplateFile "https://raw.githubusercontent.com/Kaustubhin/elkpaas/master/elkpublic.json"
+
+start-sleep 380 
+
+# Add Custom extension to Install Elk-Stack on VM1 
+Set-AzureRmVMCustomScriptExtension -ResourceGroupName $rg `
+     -VMName elkVM0 `
+     -Location "Central US" `
+     -FileUri "https://raw.githubusercontent.com/Kaustubhin/elkpaas/master/elkinstall.ps1" `
+     -Run 'elkinstall.ps1' `
+     -Name installELK-Stack
+Start-Sleep -Seconds 180
+
+
+#Add Custom extension to Install Winlogbeat on VM2
+Set-AzureRmVMCustomScriptExtension -ResourceGroupName $rg `
+     -VMName elkVM1 `
+     -Location "Central US" `
+     -FileUri "https://raw.githubusercontent.com/Kaustubhin/elkpaas/master/winlogbeatinstall.ps1" `
+     -Run 'winlogbeatinstall.ps1' `
+     -Name Winlogbeatinstall
+Start-Sleep -Seconds 60
